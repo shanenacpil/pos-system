1ead8d660bce6ebfbdf276ad236c639da3ab5899
shanenacpil <shanenacpil@users.noreply.github.com>
2025-05-14 08:39:09 -0400

    Create server.js

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.get('/', (req, res) => {
+  res.json({ message: 'POS API is live!' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
