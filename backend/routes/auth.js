e4b9a17674c0b190ae784b040f05d85acc537644
shanenacpil <shanenacpil@users.noreply.github.com>
2025-05-14 09:49:16 -0400

    Update auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,1 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const authCtrl = require('../controllers/authController');
+
+router.post('/login', authCtrl.login);
+router.post('/register', authCtrl.register);
+
+module.exports = router;
 
