a4145e70257c401f17a2795d21e1cf299465f163
shanenacpil <shanenacpil@users.noreply.github.com>
2025-05-14 15:12:59 -0400

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
-const app = express();
-const authRoutes = require('./routes/auth');
+const cors = require('cors'); // Added CORS
 
+const app = express();
+app.use(cors()); // Enabled CORS
 app.use(express.json());
+
+const authRoutes = require('./routes/auth');
 app.use('/api/auth', authRoutes);
 
 app.get('/', (req, res) => res.send('POS system is live!'));
@@ -11,4 +14,3 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
-
