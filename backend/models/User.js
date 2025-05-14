bdc656e2419079aedb6fdcae2efeb4d16cfc3962
shanenacpil <shanenacpil@users.noreply.github.com>
2025-05-14 09:47:17 -0400

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,1 +1,11 @@
+const users = [];
+
+module.exports = {
+  findByEmail: (email) => users.find(u => u.email === email),
+  createUser: (user) => {
+    users.push(user);
+    return user;
+  },
+  getAllUsers: () => users,
+};
 
