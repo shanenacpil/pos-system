8a82479ded43f9e8f2eee6fc909ce721a89af7b6
shanenacpil <shanenacpil@users.noreply.github.com>
2025-05-15 16:33:03 -0400

    Update customers.js

--- backend/routes/customers.js
+++ backend/routes/customers.js
@@ -1,1 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const customerController = require('../controllers/customerController');
+const auth = require('../middlewares/authMiddleware');
+
+// All routes are protected
+router.get('/', auth, customerController.getAll);
+router.get('/:id', auth, customerController.getById);
+router.post('/', auth, customerController.create);
+router.put('/:id', auth, customerController.update);
+router.delete('/:id', auth, customerController.remove);
+
+module.exports = router;
 
